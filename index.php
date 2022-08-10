f15a3abf34be810f53adc6255191010170cf2bdb
Rania Alablani <53059339+ran6060@users.noreply.github.com>
2022-08-10 13:52:42 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<html lang="en">
+    <head>
+        <title>Get Method</title>
+        <link rel="stylesheet" type="text/css" href="style.css"/>
+    </head>
+    <body>
+
+     <form action="db.php" method="get">
+            <button name="forward" id="forward" >Forward</button><br></br>
+            <button name="left" id="left" >Left</button>
+            <button name="stop" id="stop" >Stop</button>
+            <button name="right" id="right" >Right</button>
+    			<br> </br>
+    	<button name="backward" id="backward" >Backward</button>
+    </form>
+    </body>
+</html>
